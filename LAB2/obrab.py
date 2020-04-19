0a63eee0ffbb6317b13e9d132836c6f57c4f1e94
NP <aizeknev8@gmail.com>
2020-04-19 22:44:26 +0300

    We'll all die here at ITMO doing these labs

---
+++ LAB2/obrab.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+import numpy as np
+import matplotlib.pyplot as mpl
+from matplotlib.dates import DateFormatter
+
+
+nfcom = "nfdump -r nfcapd.202002251200 -o \"fmt:%ts,%sa,%da,%ibyt,%obyt\" | sed \"s/ //g\" | ghead -n -4 > data.csv"
+os.system(nfcom)
+  
+def sum_traf(k, Q):
+    return Q * k
+
+def graph(times, values):
+    fig, ax = mpl.subplots(figsize=[15,5])
+    ax.plot(times, values)
+    DF = DateFormatter("%H:%M:%S")
+    ax.xaxis.set_major_formatter(DF)
+    mpl.title('Зависимость объема трафика от времени')
+    mpl.xlabel('Время')
+    mpl.ylabel('Всего байт в пакете')
+    mpl.gcf().autofmt_xdate()
+    mpl.show()
+
+IP_addr = '77.74.181.52'
+koef = 1.5
+
+rdcsv = pd.read_csv('data.csv', skiprows=1, header=None)
+

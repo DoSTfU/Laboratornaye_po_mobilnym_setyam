5c3967963c5ebaab2c3e80a09f8f09eb3db42116
NP <aizeknev8@gmail.com>
2020-04-19 22:33:39 +0300

    We'll all die

--- LAB2/obrab.py
+++ LAB2/obrab.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as mpl
 from matplotlib.dates import DateFormatter
 
 
@@ -11,19 +11,16 @@ os.system(nfcom)
 def sum_traf(k, Q):
     return Q * k
 
-def graph(times, values, format=None, save_to=None):
-    fig, ax = plt.subplots(figsize=[15,5])
+def graph(times, values, format=None):
+    fig, ax = mpl.subplots(figsize=[15,5])
     ax.plot(times, values)
-    myFmt = DateFormatter(format if format is not None else "%H:%M:%S")
-    ax.xaxis.set_major_formatter(myFmt)
-    plt.gcf().autofmt_xdate()
-    plt.ylabel('Всего байт в пакете')
-    plt.xlabel('Время')
-    plt.title('Зависимость объема трафика от времени')
-    if save_to:
-        plt.save(save_to)
-    else:
-        plt.show()
+    DF = DateFormatter(format if format is not None else "%H:%M:%S")
+    ax.xaxis.set_major_formatter(DF)
+    mpl.title('Зависимость объема трафика от времени')
+    mpl.xlabel('Время')
+    mpl.ylabel('Всего байт в пакете')
+    mpl.gcf().autofmt_xdate()
+    mpl.show()
 
 IP_addr = '77.74.181.52'
 koef = 1.5

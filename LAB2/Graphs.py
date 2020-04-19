dd90b1059e5f11707e92f038b805b2bc735ce61c
NP <aizeknev8@gmail.com>
2020-04-19 22:42:09 +0300

    .

--- LAB2/Graphs.py
+++
@@ -1,9 +0,0 @@
-import obrab as obr
-import tarif as trf 
-
-times = obr.np.sort(trf.rc.t.unique())
-values = []
-for t in times:
-    values.append(trf.rc.loc[trf.rc.t == t, ['ibys', 'obys']].sum().sum())
-
-obr.graph(obr.pd.to_datetime(times), values)
